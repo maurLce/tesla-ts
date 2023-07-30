6182524c80966b4b6a1f921a1a201b987f6475c7
maurLce <10817256+maurLce@users.noreply.github.com>
2023-07-29 23:14:12 -0400

    Update API host URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_HOST = "";
+export const API_HOST = "/owner-api";
 export const API_URL = `${API_HOST}/api/1`;
 export const STREAM_HOST = "streaming.vn.teslamotors.com";
 export const STREAM_URL = `wss://${STREAM_HOST}/streaming/`;
